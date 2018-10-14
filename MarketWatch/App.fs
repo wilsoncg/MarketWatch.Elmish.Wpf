fa0c52da723b4babb1e11b43de9439102549ba78
Craig Wilson <craig@wilsoncg.net>
2018-10-14 20:21:19 +0100

    not blank

---
+++ MarketWatch/App.fs
@@ -0,0 +1,51 @@
+﻿module App
+
+open Elmish.WPF
+
+module ListItem =
+  
+ type Market = { 
+    Id: int
+    Name: string
+    Price: decimal
+    Open: decimal
+    Close: decimal }
+    
+ let newMarket id price = {
+    Id = id
+    Name = ""
+    Price = price
+    Open = 0m
+    Close = 0m
+ }
+
+ let rec bindings() =
+  [
+    "Name" |> Binding.oneWay (fun m -> m.Name)
+    "Price" |> Binding.oneWay (fun m -> m.Price)
+  ] 
+
+module List =
+
+ type Model = {
+    Markets: list<ListItem.Market> }
+    
+ let init() = {
+    Markets = Seq.empty<ListItem.Market> |> Seq.toList }
+
+ type Msg =
+    | UpdatePrice of int * decimal
+    | ParentMsg of int * Msg
+
+ let update msg m =
+    match msg with
+    | UpdatePrice (id, price) -> { m with Markets = list.Cons ((ListItem.newMarket id price), m.Markets) }
+
+ let bindings model dispatch =
+  [
+    "Markets" |> Binding.subModelSeq 
+        (fun m -> m.Markets)
+        (fun cm -> cm.Id)
+        (fun () -> ListItem.bindings ())
+        ParentMsg
+  ]
---
+++ MarketWatch/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+open Elmish
+open Elmish.WPF
+open App
+
+[<EntryPoint; STAThread>]
+let main argv =
+  Program.mkSimple App.List.init App.List.update App.List.bindings
+  |> Program.withConsoleTrace
+  |> Program.runWindowWithConfig
+      { ElmConfig.Default with LogConsole = true }
+      (WpfControls.AppWindow())
